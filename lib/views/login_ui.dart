14f38652c9cab41b9c162bc949f69408b721094b
PrQnHuBer <mokil05zaza@hotmail.com>
2025-03-05 22:02:14 +0700

    เพิ่งเริ่ม

---
+++ lib/views/login_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class loginUi extends StatefulWidget {
+  const loginUi({super.key});
+
+  @override
+  State<loginUi> createState() => LoginUi();
+}
+
+class LoginUi extends State<loginUi> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
